bf9145116e998955ee425ef249582fc5d7e19e4b
Mac <mac@Macs-MacBook-Pro.local>
2017-02-06 21:27:43 +0800

    Added future test methods for extensibility

---
+++ test/internal-notifier-specs.js
@@ -0,0 +1,61 @@
+/*****************************************************
+ * Internal tests
+ * What are internal tests?
+ * As this is a npm package, it should be tested from
+ * a package context, so I'll use "interal" preffix
+ * for tests which are NOT using the npm tarball pack
+ * For all others, the test should obviously include
+ * something like:
+ * var md = require('t-motion-detector');
+ *****************************************************/
+
+var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+var should = chai.should();
+var fs = require('fs');
+var ent = require('../Entities.js');
+var main = require('../main.js');
+var events = require('events');
+
+//Chai will use promises for async events
+chai.use(chaiAsPromised);
+
+before(function(done) {
+  done();
+});
+
+after(function(done) {
+  // here you can clear fixtures, etc.
+  done();
+});
+
+describe("When a new motion detector is created, ", function() {
+  it('there should be a default environment associated', function () {
+    //Prepare
+    main.Start();
+    main.GetEnvironment().should.not.equal(undefined);
+  });
+
+  it('all notifiers are binded with the motion Detectors', function (done) {
+    //Prepare
+
+    var n = new main.BaseNotifier();
+    var e = new ent.Environment();
+    var m = new ent.MotionDetector();
+
+    n.on('pushedNotification', function(message, text){
+        console.log("A new notification has arrived!", message, text);
+        text.should.equal("Started");
+        done();
+    })
+
+    var result = false;
+    main.Start({
+      environment: e,
+      initialNotifier: n,
+      initialMotionDetector: m
+    });
+
+    e.AddChange(10);
+  });
+});
